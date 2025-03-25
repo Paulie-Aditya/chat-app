2a33a1aac9f129898aad3c449f1f3bd4115dff25
Paulie-Aditya <paul.aditya304@gmail.com>
2025-03-26 03:09:40 +0530

    message controllers done

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from "express"
 import dotenv from "dotenv"
-import authRoutes from './routes/auth.routers.js'
+import cookieParser from "cookie-parser"
+import authRoutes from './routes/auth.routes.js'
+import messageRoutes from './routes/message.routes.js'
 import connectToMongoDB from "./db/connectToMongoDB.js"
 
 dotenv.config()
@@ -9,7 +11,9 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.use(express.json()); // to parse input from req body
+app.use(cookieParser())
 app.use("/api/auth", authRoutes)
+app.use("/api/messages", messageRoutes)
 
 
 app.listen(PORT, ()=> {
